9d204c3e097b265d775e0c7d3a9495bb51b9ab03
Chiefwiggy <kcollin0320@gmail.com>
2024-07-13 00:41:00 -0500

    rerun

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,46 @@
-import {app, router} from './app'
-import mongoose from "mongoose";
+import express, {Router} from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import CharacterRouter from "./Routers/CharacterRouter";
+import CardRouter from "./Routers/CardRouter";
+import AuthRouter from "./Routers/AuthRouter";
+import EquipmentRouter from "./Routers/Equipment/EquipmentRouter";
+import AbilityRouter from "./Routers/AbilityRouter";
+import UserRouter from "./Routers/UserRouter";
+import ClassRouter from "./Routers/ClassRouter";
+
+import serverless from 'serverless-http';
+
+const PORT: number = Number(process.env.PORT) || 3001;
+
+const app = express();
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 mongoose.connect(process.env.MONGO_URL ?? "").catch((err) => {
     console.error("Error: could not connect to mongodb")
 })
 
-const port = process.env.PORT || 3001;
+const router = Router();
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running`);
-});
+router.use("/characters", CharacterRouter);
+router.use('/cards', CardRouter);
+router.use('/auth', AuthRouter);
+router.use("/equipment", EquipmentRouter);
+router.use("/abilities", AbilityRouter);
+router.use("/users", UserRouter);
+router.use("/classes", ClassRouter);
+
+router.get("/", (req, res) => {
+    res.send("hello");
+})
+
+app.use('/.netlify/functions/api', router);
+
+
+export const handler = serverless(app);
--- src/netlify/functions/index.ts
+++
@@ -1,7 +0,0 @@
-import serverless from "serverless-http";
-
-import {app, router} from '../../app'
-
-app.use('/', router);
-
-export const handler = serverless(app);
