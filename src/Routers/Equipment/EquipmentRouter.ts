d0151ff9eb76b72a868b397e1756059546c04a77
Chiefwiggy <kcollin0320@gmail.com>
2024-08-30 14:13:24 -0500

    add consumable router

--- src/Routers/Equipment/EquipmentRouter.ts
+++ src/Routers/Equipment/EquipmentRouter.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express'
 import ArmorRouter from "./ArmorRouter";
 import ConsumableModel from "../../Models/Equipment/ConsumableModel";
+import ConsumableRouter from "./ConsumableRouter";
 
 
 const router = Router();
 
 router.use("/armor", ArmorRouter);
-router.use("/consumable", ConsumableModel)
+router.use("/consumable", ConsumableRouter)
 
 export default router;
