a749f7f5877b6155df178d430263e30dabae30da
Chiefwiggy <kcollin0320@gmail.com>
2024-10-08 17:26:33 -0500

    new enums

--- src/Enums/EquipmentEnums.ts
+++ src/Enums/EquipmentEnums.ts
@@ -2,4 +2,4 @@
 
 export const EArmorClass = ["light", "standard", "heavy"]
 
-export const EConsumableType = ["healing", "bomb", "buff", "trap", "totem", "flag", "curse"]
+export const EConsumableType = ["healing", "bomb", "buff", "tracd p", "totem", "flag", "curse"]
