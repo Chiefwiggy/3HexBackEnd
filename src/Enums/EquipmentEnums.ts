64ba4523170672e0bd6de370bde76e4b65439beb
Chiefwiggy <kcollin0320@gmail.com>
2024-11-25 00:39:34 -0600

    equip

--- src/Enums/EquipmentEnums.ts
+++ src/Enums/EquipmentEnums.ts
@@ -2,4 +2,6 @@
 
 export const EArmorClass = ["light", "standard", "heavy"]
 
-export const EConsumableType = ["healing", "bomb", "buff", "trap", "totem", "flag", "curse"]
+export const EConsumableType = ["healing", "buff", "damage", "debuff", "container", "support"]
+
+export const EConsumableCraftingType = ["potion", "trap", "totem", "curse", "holy", "flag", "bomb", "none", "gem", "medical"]
--- src/Models/Equipment/ConsumableModel.ts
+++ src/Models/Equipment/ConsumableModel.ts
@@ -1,31 +1,56 @@
 import mongoose from 'mongoose';
 import {ESaveTypes} from "../../Enums/CardEnums";
-import {EConsumableType} from "../../Enums/EquipmentEnums";
+import {EConsumableCraftingType, EConsumableType} from "../../Enums/EquipmentEnums";
+import {IScalingData} from "../Cards/BaseWeaponCardModel";
 
 export interface _IConsumable {
     itemName: string,
-    basePower: number,
-    potency: number,
-    skillScaling: string,
-    tetherCost?: number,
     itemType: string,
+    craftingType: string,
+    tetherCost: number,
     description: Array<string>,
+    xVals: Array<{
+        basePower: number,
+        potency: number,
+        skillScaling: string,
+        abilityScaling: {
+            modifiers?: Array<string>,
+            multipliers?: Array<string>,
+            overrides?: Array<string>
+        }
+    }>,
     slotCost: number,
     itemTier: number,
-    itemBaseHealth?: number
+    itemCost: number,
+    materialCost: number
 }
 
 const ConsumableSchema = new mongoose.Schema<_IConsumable>({
     itemName: {type: String, required: true, unique: true},
-    basePower: {type: Number, required: true, default: 0},
-    potency: {type: Number, required: true, default: 0},
-    skillScaling: {type: String, required: true, default: "none", enum: ESaveTypes},
-    tetherCost: {type: Number, required: false},
     itemType: {type: String, required: true, enum: EConsumableType},
+    craftingType: {type: String, enum: EConsumableCraftingType},
+    tetherCost: {type: Number, required: false},
     description: {type: [String], required: true},
+    xVals: [
+        {
+            basePower: {type: Number, required: true, default: 0},
+            potency: {type: Number, required: true, default: 0},
+            skillScaling: {type: String, required: true, default: "none", enum: ESaveTypes},
+            abilityScaling: {
+                type: {
+                    modifiers: [String],
+                    multipliers: [String],
+                    overrides: [String],
+                },
+                required: true,
+                default: {}
+            }
+        }
+    ],
     slotCost: {type: Number, required: true, default: 1},
     itemTier: {type: Number, required: true, default: 1},
-    itemBaseHealth: Number
+    itemCost: {type: Number, required: true, default: 100},
+    materialCost: {type: Number, required: true, default: 25}
 })
 
 const ConsumableModel = mongoose.model("consumables", ConsumableSchema);
