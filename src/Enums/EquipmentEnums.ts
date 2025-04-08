4cdcde11de472fb5557de7dabd6c3cfdb8a967ea
Chiefwiggy <kcollin0320@gmail.com>
2025-04-07 19:27:13 -0500

    add enum

--- src/Enums/EquipmentEnums.ts
+++ src/Enums/EquipmentEnums.ts
@@ -4,4 +4,4 @@ export const EArmorClass = ["light", "standard", "heavy"]
 
 export const EConsumableType = ["healing", "buff", "damage", "debuff", "container", "support"]
 
-export const EConsumableCraftingType = ["potion", "trap", "totem", "curse", "holy", "flag", "bomb", "none", "gem", "medical"]
+export const EConsumableCraftingType = ["potion", "poison", "oil", "trap", "totem", "curse", "holy", "flag", "bomb", "none", "gem", "medical"]
