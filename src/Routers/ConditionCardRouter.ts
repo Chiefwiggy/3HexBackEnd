a3696772d00240f202b61ec199c84c4a48d07523
Chiefwiggy <kcollin0320@gmail.com>
2025-12-17 01:31:38 -0600

    Update ConditionCardRouter.ts

--- src/Routers/ConditionCardRouter.ts
+++ src/Routers/ConditionCardRouter.ts
@@ -4,11 +4,10 @@ import {Router} from 'express';
 import {AddCommanderCard, GetAllCommanderCards} from "../Controllers/CommanderCardController";
 import {AddConditionCard} from "../Controllers/AddCardsController";
 import {GetAllConditionCards} from "../Controllers/GetCardDataController";
-import {EditCommanderCard} from "../../Controllers/EditCardsController";
+
 
 const router = Router();
 
 router.get("/getAll", GetAllConditionCards)
 router.post("/add", AddConditionCard)
-router.put("/update/:id", EditCommanderCard) 
 export default router;
