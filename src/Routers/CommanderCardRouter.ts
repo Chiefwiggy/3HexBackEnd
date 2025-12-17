40e5722eb5c3adcaa1f81904b910bdbf470f6b84
Chiefwiggy <kcollin0320@gmail.com>
2025-12-17 01:31:58 -0600

    Update CommanderCardRouter.ts

--- src/Routers/CommanderCardRouter.ts
+++ src/Routers/CommanderCardRouter.ts
@@ -2,9 +2,11 @@
 
 import {Router} from 'express';
 import {AddCommanderCard, GetAllCommanderCards} from "../Controllers/CommanderCardController";
-
+import {EditCommanderCard} from "../../Controllers/EditCardsController";
 const router = Router();
 
 router.get("/getAll", GetAllCommanderCards)
 router.post("/add", AddCommanderCard)
+
+router.put("/update/:id", EditCommanderCard) 
 export default router;
